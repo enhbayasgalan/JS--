217e187ccfefc9929e34bd9714252f6404d75dd4
unknown <enhbayasgalan@gmail.com>
2025-07-21 12:29:07 +0800

    JS1

---
+++ index.js
@@ -0,0 +1,196 @@
+// function reverse(str) {
+//     return str.split('').reverse().join('')
+// }
+
+// console.log(reverse("hello"));
+
+//split('') -> string-ийг array болгоно
+//reverse('') -> ["o", "l", "l", "e", "h"]
+
+
+// task-2 
+// function sumArray(arr) {
+//     let result = 0; // нийлбэр хадгалах хувьсагч
+//     for(let i = 0; i < arr.length; i++) {
+//         result += arr[i]
+//     }
+//     return result;
+// }
+
+// console.log(sumArray([1, 2, 3]));
+
+//Хэт богино хувилбар
+// const sumArray = arr => arr.reduce((sum, curr) => sum + curr, 0)
+
+
+// task-3
+// function charFrequency(str) {
+//     let result = {}  // хоосон объект түлхүүр = үсэг, утга = хэдэн удаа гарсан
+//     for(let char of str) { // for...of loop ашиглан str-ийн үсэг бүрийг нэг нэгээр нь авч байгаа.
+//         if (result[char]) { // Хэрвээ тухайн char (үсэг) аль хэдийн result дотор байгаа бол, +1 гэж нэмэгдүүлнэ
+//             result[char]++;
+//         } else {
+//             result[char] = 1; // Хэрвээ тухайн үсэг анх удаа гарч байгаа бол, 1 гэж онооно (эхэлж байна гэсэн үг)
+//         } 
+//     }
+    
+//     return result // result объектыг буцаана
+// }
+
+// console.log(charFrequency("hello"));
+
+
+// task-4
+// function isAnagram(s, t) {
+//     if (s.length !== t.length) {
+//         return false // Хэрвээ s ба t-ийн урт өөр байвал anagram байж болохгүй тул шууд false буцаана.
+//     }
+//     function charFrequency(str) {
+//         let freq = {}
+//         for (let char of str) {
+//             freq[char] = freq[char] ? freq[char] + 1 : 1;
+//         }
+//         return freq; // Үсгийн давтамж тоолох дэд функц
+//     }
+//     const freqS = charFrequency(s)
+//     const freqT = charFrequency(t)
+
+//     for(let char in freqS) { // Хоёр объектыг харьцуулах
+//         if (freqS[char] !== freqT[char]) {
+//             return false // Хэрвээ аль нэг үсгийн давтамж зөрүүтэй байвал false буцаана.
+//         }
+//     }
+
+//     return true  // Бүх үсэг ижил тоотой байвал true буцаана.
+// }
+
+// console.log(isAnagram("listen", "silent"));     
+
+
+// task-5
+// function checkDoubleExists(arr) {
+//     const seen = new Set();
+
+//     for(let num of arr) {
+//         if (seen.has(num * 2) || seen.has(num/2)) {
+//             return true
+//         }
+//         seen.add(num);
+//     }
+
+//     return false
+// }
+
+// console.log(checkDoubleExists([10, 2, 5, 3]));
+
+
+// Easy-level logic
+// function countVowels(str) {
+//     let count = 0;
+//     const vowels = "aeiou"
+
+//     for (let char of str.toLowerCase()) { // эгшиг мөн эсэхийг шалгана
+//         if (vowels.includes(char)) {  // Том жижиг үсэг ялгахгүй болно 
+//             count++; // эгшиг таарах бүрд тоогоо нэмнэ
+//         }
+//     }
+//     return count
+// }
+// console.log(countVowels("hello"));
+
+
+// Өгсөн тоонуудын массив дотроос хамгийн их утгыг олоорой.
+// function findMax(arr) {
+//     let max = arr[0]
+
+//     for(let i = 1; i < arr.length; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i];
+//         }
+//     }
+
+//     return max
+// }
+
+// console.log(findMax([1, 5, 3, 9, 2]));
+
+
+// function countVowels(str) {
+//     let count = 0;
+//     const vowels = "aeiou"
+
+//     for(let char of str.toLowerCase()) {
+//         if (vowels.includes(char)) {
+//             count++
+//         }
+//     }
+//     return count
+// }
+
+// console.log(countVowels("hello"));
+
+
+// Reverse Number
+// function reverseNumber(num) {
+//     const reversedStr = Math.abs(num).toString().split('').reverse().join('')
+//     const reverseNum = parseInt(reversedStr)
+//     return num < 0 ? -reverseNum : reverseNum;
+// }   
+// Math.abs(num) → сөрөг тэмдгийг түр салгаж авна.
+// .toString().split('').reverse().join('') → тоог string болгож эргүүлнэ.
+// parseInt(...) → эргүүлсэн string-ээ тоо болгон буцаана.
+// Эцэст нь num < 0 бол сөрөг тэмдэг дахин тавина.
+
+// console.log(reverseNumber(123)); 
+
+
+// Class Logic Changalle "Fizzbuzz"
+// function Fizzbuzz(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz");
+//         } else if (i % 3 === 0) {
+//             console.log("Fizz");
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// Fizzbuzz(15)
+
+
+// Palindrome 
+
+// function isPalindrome(str) {
+//     const reverse = str.split('').reverse().join('')
+//     return str === reverse
+// }
+
+// console.log(isPalindrome("madam"));
+
+
+
+// Remove Duplicates
+// function removeDuplicates(arr) {
+//     const result = [];
+//     for (i = 0; i < arr.length; i++) {
+//         if (!result.includes(arr[i])) {
+//             result.push(arr[i]);
+//         }
+//     }
+//     return result
+// }
+// console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+
+
+//  Find the Second Largest Number
+// function secondLargest(arr) {
+//     const max = Math.max(...arr)
+//     const filtered = arr.filter(n => n !== max)
+//     return Math.max(...filtered)
+// }
+
+// console.log(secondLargest([1, 3, 7, 4, 6]));
